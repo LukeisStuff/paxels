435766c3007cc929c662ea750f85cc8d867ab293
Artem <artemta3@gmx.de>
2025-12-12 00:42:37 +0100

    Change implementation dependencies to compileOnly

--- build.gradle.kts
+++ build.gradle.kts
@@ -70,11 +70,11 @@ dependencies {
 
     implementation(libs.moon)
 
-    implementation(libs.aether)
-    implementation(libs.dragonfly)
-    implementation(libs.catalyst.core)
-    implementation(libs.catalyst.effects)
-    implementation(libs.uselessNumerical.get().let { "${it.group}:${it.name}:${it.version}-${libs.versions.bta.get()}" })
+    compileOnly(libs.aether)
+    compileOnly(libs.dragonfly)
+    compileOnly(libs.catalyst.core)
+    compileOnly(libs.catalyst.effects)
+    compileOnly(libs.uselessNumerical.get().let { "${it.group}:${it.name}:${it.version}-${libs.versions.bta.get()}" })
 
     runtimeOnly(libs.clientJar)
     implementation(libs.loader)
