ae236e43ac36ba3217ada96218a4f000243bf30e
Redart15 <redart15@users.noreply.github.com>
2026-08-02 13:50:35 +0200

    added paxel back to the creative inventory

--- src/main/java/luke/paxels/PaxelItems.java
+++ src/main/java/luke/paxels/PaxelItems.java
@@ -1,9 +1,12 @@
 package luke.paxels;
 
 import net.minecraft.core.item.Item;
+import net.minecraft.core.item.Items;
 import net.minecraft.core.item.material.ToolMaterial;
 import net.minecraft.core.item.tag.ItemTags;
 import turniplabs.halplibe.helper.ItemBuilder;
+import turniplabs.halplibe.helper.creativeInventory.CreativeInventoryCategory;
+import turniplabs.halplibe.helper.creativeInventory.CreativeInventoryPlacement;
 
 import static luke.paxels.PaxelMod.MOD_ID;
 
@@ -33,22 +36,28 @@ public final class PaxelItems {
 
     public static void initializeItems() {
         TOOL_PAXEL_WOOD = new ItemBuilder(MOD_ID)
+            .setCreativeInventoryPlacement(new CreativeInventoryPlacement.After(() -> Items.TOOL_SWORD_WOOD))
             .build(new ItemToolPaxel("tool.paxel.wood", itemKey("tool_paxel_wood"), 20000, ToolMaterial.wood));
 
         TOOL_PAXEL_STONE = new ItemBuilder(MOD_ID)
+            .setCreativeInventoryPlacement(new CreativeInventoryPlacement.After(() -> Items.TOOL_SWORD_STONE))
             .build(new ItemToolPaxel("tool.paxel.stone", itemKey("tool_paxel_stone"), 20001,ToolMaterial.stone));
 
         TOOL_PAXEL_IRON = new ItemBuilder(MOD_ID)
+            .setCreativeInventoryPlacement(new CreativeInventoryPlacement.After(() -> Items.TOOL_SWORD_IRON))
             .build(new ItemToolPaxel("tool.paxel.iron", itemKey("tool_paxel_iron"), 20002, ToolMaterial.iron));
 
         TOOL_PAXEL_GOLD = new ItemBuilder(MOD_ID)
             .setTags(ItemTags.IS_SILK_TOUCH)
+            .setCreativeInventoryPlacement(new CreativeInventoryPlacement.After(() -> Items.TOOL_SWORD_GOLD))
             .build(new ItemToolPaxel("tool.paxel.gold", itemKey("tool_paxel_gold"), 20003, ToolMaterial.gold));
 
         TOOL_PAXEL_DIAMOND = new ItemBuilder(MOD_ID)
+            .setCreativeInventoryPlacement(new CreativeInventoryPlacement.After(() -> Items.TOOL_SWORD_DIAMOND))
             .build(new ItemToolPaxel("tool.paxel.diamond", itemKey("tool_paxel_diamond"), 20004, ToolMaterial.diamond));
 
         TOOL_PAXEL_STEEL = new ItemBuilder(MOD_ID)
+            .setCreativeInventoryPlacement(new CreativeInventoryPlacement.After(() -> Items.TOOL_SWORD_STEEL))
             .build(new ItemToolPaxel("tool.paxel.steel", itemKey("tool_paxel_steel"), 20005, ToolMaterial.steel));
     }
 
--- src/main/java/luke/paxels/PaxelMod.java
+++ src/main/java/luke/paxels/PaxelMod.java
@@ -4,12 +4,13 @@ import net.fabricmc.api.ModInitializer;
 import net.minecraft.core.crafting.LookupFuelFurnace;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import turniplabs.halplibe.HalpLibe;
 import turniplabs.halplibe.event.defs.CommonEvents;
 import turniplabs.halplibe.util.dependency.Key;
 
 
 public class PaxelMod implements ModInitializer {
-    public static final String MOD_ID = "paxels";
+    public static final String MOD_ID = HalpLibe.registerMod("paxels");
     public static final Key KEY = Key.of(MOD_ID);
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
